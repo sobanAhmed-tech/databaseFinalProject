3ec6c97bd2843b77a5a2bfd53829dad2007c4107
sobanAhmed-tech <sobanahmed23200@gmail.com>
2024-11-25 15:10:59 +0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,6 +14,7 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, "../client/public")));
 
 // Configure session middleware
+// hellioejkrhfjkerhfjkenrjk
 app.use(
   session({
     secret: "your_secret_key",
